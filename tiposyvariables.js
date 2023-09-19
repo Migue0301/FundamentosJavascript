761fccb6eecef152d9af13c58533fe234b34b109
Migue0301 <miguel.paredesperez1@gmail.com>
2023-09-18 19:33:03 -0600

    02 Variables y Tipo de Datos

---
+++ tiposyvariables.js
@@ -0,0 +1,35 @@
+// Tipos en java script 
+// number, string, boolean, object, undefined
+// typeof nos dice el tipo de dato
+console.log(12);
+console.log(typeof(12));
+
+console.log('string');
+console.log(typeof('string'));
+
+console.log(true);
+console.log(typeof(true));
+
+let variable;
+console.log(typeof(variable));
+
+console.log([1, 2, 3]);
+console.log(typeof([1, 2, 3]));
+
+// Variables en Javscript
+// versiones anteriores ES6 var
+var firstName = 'Tony Stark';
+
+// const y let de ES6 en adelante
+// constantes const (variables de solo lectura)
+// Siempre debe inicializarse
+// No permite reasignacion 
+
+const PI = 3.1416;
+
+// Variables con let 
+// declarar
+let lastName;
+lastName = 'Pérez'
+lastName = true;
+lastName = 25;
