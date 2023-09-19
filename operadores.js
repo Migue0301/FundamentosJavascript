ddeef38a2861b332220de4298cc885c0ff497f06
Migue0301 <miguel.paredesperez1@gmail.com>
2023-09-18 19:34:35 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,35 @@
+// Operadores Matemáticos
+// + - * / % 
+// += -= *= /= %= 
+ console.log(10 + 10);
+ console.log(10 - 10);
+ console.log(10 / 10);
+ console.log(10 * 10);
+ console.log(10 % 10);
+
+
+ let num = 15;
+ console.log(num += 5); // num = num + 5
+ console.log(num -= 5);
+ console.log(num /= 5);
+ console.log(num *= 5);
+ console.log(num %= 5);
+
+ // operadores booleanos
+ // > < >= <= !== ===
+ console.log(1 > 2);
+ console.log(1 < 2);
+ console.log(1 >= 2);
+ console.log(1 <= 2);
+ console.log(1 !== 2);
+ console.log(1 === 2);
+
+// Operadores logicos 
+// and &&
+console.log(true && true);
+// or ||
+console.log(true || false);
+// not !
+console.log(!true);
+
+
